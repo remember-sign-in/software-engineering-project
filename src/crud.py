28fdb1aa970a149d4f3c64739a64bcdfaf8f28b9
Jiyiajia <148039839+Jiyiajia@users.noreply.github.com>
2023-12-15 10:46:53 +0800

    6个接口 (#37)

--- src/crud.py
+++ src/crud.py
@@ -6,6 +6,7 @@ import models
 def get_user_by_openid(db: Session, openid: str) -> models.User:
     return db.query(models.User).filter(models.User.open_id == openid).first()
 
+
 # 创建用户
 def create_user(db: Session, openid: str) -> models.User:
     db_user = models.User(open_id=openid)
@@ -13,3 +14,62 @@ def create_user(db: Session, openid: str) -> models.User:
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+# 查询用户创建的班级
+def get_my_class(db: Session, user_id: str) -> models.MyClass:
+    return db.query(models.MyClass).filter(models.MyClass.user_id == user_id).all()
+
+
+# 查询用户加入的班级
+def get_join_class(db: Session, user_id: str) -> models.MyClass:
+    db_joinclass_id = db.query(models.JoinClass).filter(models.JoinClass.user_id == user_id).all()
+    if not db_joinclass_id: return None
+    return db.query(models.MyClass).filter(models.MyClass.class_id == db_joinclass_id.class_id).all()
+
+
+# 创建新班级
+def create_class(db: Session, creator_id: str, name: str, joinCode: str, stuNum: int) -> models.MyClass:
+    db_myclass = models.MyClass(user_id=creator_id, class_name=name, numbers=stuNum, joinCode=joinCode)
+    db.add(db_myclass)
+    db.commit()
+    db.refresh(db_myclass)
+    return db_myclass
+
+
+# 获取目标id的班级学生列表
+def get_class_list(db: Session, id: str) -> models.User:
+    db_joinclass = db.query(models.JoinClass).filter(models.JoinClass.class_id == id).all()
+    if not db_joinclass:
+        return None
+    return db.query(models.User).filter(models.User.user_id == db.joinclass.user_id).all()
+
+
+# 加入班级
+def join_class(db: Session, id: str, joinCode: str) -> int:
+    db_myclass = db.query(models.MyClass).filter(models.MyClass.joinCode == joinCode).first()
+    if not db_myclass:
+        return 0
+    if not db.query(models.JoinClass).filter(
+            models.JoinClass.user_id == id and models.JoinClass.class_id == db_myclass.class_id).first():
+        db_joinclass = models.JoinClass(user_id=id, class_id=db_myclass.class_id)
+        db.add(db_joinclass)
+        db.commit()
+        db.refresh(db_joinclass)
+        return 1
+    return -1
+
+
+# 删除班级
+def delete_class(db: Session, class_id: str) -> int:
+    db_class = db.query(models.MyClass).filter(models.MyClass.class_id == class_id).first()
+    if not db_class:
+        return 0
+    db.delete(db_class)
+    db.commit()
+    # 删除选该门课程的学生
+    db_students = db.query(models.JoinClass).filter(models.JoinClass.class_id == class_id).all()
+    for student in db_students:
+        db.delete(student)
+    db.commit()
+    return 1
--- src/models.py
+++ src/models.py
@@ -8,3 +8,20 @@ class User(Base):
         Integer, primary_key=True, index=True, autoincrement=True, nullable=False
     )
     open_id = Column(String(40), index=True, nullable=False)
+    name = Column(String(15), index=True, nullable=False)
+    admin_class = Column(String(15), index=True, nullable=False)
+
+
+class MyClass(Base):
+    __tablename__ = "MyClass"
+    class_id = Column(Integer, primary_key=True, index=True, autoincrement=True, nullable=False)
+    user_id = Column(String(15), index=True, nullable=False)
+    class_name = Column(String(15), index=True, nullable=False)
+    numbers = Column(Integer, index=True, nullable=False)
+    joinCode = Column(String(15), index=True, nullable=False)
+
+
+class JoinClass(Base):
+    __tablename__ = "JoinClass"
+    class_id = Column(String(15), primary_key=True, index=True, nullable=False)
+    user_id = Column(String(15), primary_key=True, index=True, nullable=False)
