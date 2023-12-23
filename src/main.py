14c16e21ad47f832c687d92ef7bac4bb3b1cde23
Chan-cy <58013852+Chan-cy@users.noreply.github.com>
2023-12-23 21:15:37 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -92,7 +92,6 @@ async def searchList(class_name: str, db: Session = Depends(get_db)):
         return responses.JSONResponse(content={"items": "null"})
     result = responses.JSONResponse(content={"items": [
         {"index": item.class_id, "name": item.class_name, "id": item.id} for item in db_searchclass]})
-         "name": item.class_name, "id": item.creator_id}] for item in db_joinclass})
     return result
 
 
